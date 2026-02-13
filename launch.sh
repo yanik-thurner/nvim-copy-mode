2ba95bd0bea1b4c421f35a3fb7db8294665cb42b
Yanik Thurner <yanik@thurner.dev>
2026-02-13 14:40:51 +0100

    uses dynamic path of launch script instead of relative one

--- launch.sh
+++ launch.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 read -r pane_id cursor_x cursor_y history_size pane_width pane_height \
   <<< "$(tmux display -p '#{pane_id} #{cursor_x} #{cursor_y} #{history_size} #{pane_width} #{pane_height}')"
 
@@ -11,4 +13,4 @@ trap cleanup EXIT
 tmux capture-pane -t "$pane_id" -p -e -N -S - -E - >> "$filename"
 tmux capture-pane -t "$pane_id" -p -e | head -n -1 > "$curtain"
 
-tmux display-popup -xP -yP -w "$pane_width" -h "$pane_height" -EE -B "cat $curtain; exec env CURSOR_Y=$cursor_y CURSOR_X=$cursor_x COPY_FILE=$filename HISTORY_SIZE=$history_size nvim --cmd 'set lazyredraw' --clean -u ./init.lua -c 'terminal cat $curtain'"
+tmux display-popup -xP -yP -w "$pane_width" -h "$pane_height" -EE -B "cat $curtain; exec env CURSOR_Y=$cursor_y CURSOR_X=$cursor_x COPY_FILE=$filename HISTORY_SIZE=$history_size nvim --cmd 'set lazyredraw' --clean -u \"$CURRENT_DIR\"/init.lua -c 'terminal cat $curtain'"
