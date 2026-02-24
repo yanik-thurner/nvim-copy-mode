bbe8607e053a737b2285d04f34aab260d8ce282e
Yanik Thurner <yanik@thurner.dev>
2026-02-24 17:20:10 +0100

    conditionally adds a space on the end of the cursor line to prevent cursor jumping

--- launch.sh
+++ launch.sh
@@ -13,4 +13,10 @@ trap cleanup EXIT
 tmux capture-pane -t "$pane_id" -p -e -S - -E - | sed $'s/$/\033[0m/' >> "$filename"
 tmux capture-pane -t "$pane_id" -p -e | head -n -1 > "$curtain"
 
+# captured text doesn't include trailing space in the line of the shell prompt, so we add one manually
+if [ "$cursor_x" -lt "$((pane_width - 1))" ]; then
+    cursor_line=$((history_size + cursor_y + 1))
+    sed -i "${cursor_line}s/$/ /" "$filename"
+fi
+
 tmux display-popup -xP -yP -w "$pane_width" -h "$pane_height" -EE -B "cat $curtain; exec env CURSOR_Y=$cursor_y CURSOR_X=$cursor_x COPY_FILE=$filename HISTORY_SIZE=$history_size nvim --cmd 'set lazyredraw' --clean -u \"$CURRENT_DIR\"/init.lua -c 'terminal cat $curtain'"
